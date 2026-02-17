f33fcf2ae3a2b4854cba3a190a5cfb8cbc05d363
ceyokota <ceyokota@ucdavis.edu>
2026-02-17 01:02:39 -0800

    added experiment 1

---
+++ everypicture/experiment/script.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+
+    const container = document.querySelector('#container');
+    const hotSpots = document.querySelectorAll('#container div');
+    const theImg = document.querySelector('div img');
+
+    hotSpots.forEach(function (eachSpot){
+        eachSpot.addEventListener('mouseover', zoomPhoto);
+        eachSpot.addEventListener('mouseout', function(){
+            theImg.className = 'start';
+        });
+
+    });
+
+    function zoomPhoto(event) {
+        const thisCorner = event.target.id;
+        console.log(thisCorner);
+        switch (thisCorner){
+            case 'coup': theImg.className = 'coupchange'; break;
+            case 'shadowhunters': theImg.className = 'shadowhunterschange'; break;
+            case 'dominion': theImg.className = 'dominionchange'; break;
+            // case 'bottomright': theImg.className = 'bottomright'; break;
+            // case 'center': theImg.className = 'center'; break;
+        }
+    }
+
+})();
