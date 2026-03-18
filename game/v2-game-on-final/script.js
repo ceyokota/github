47b9787ab66a89eb75399b0931e7aa6f94047381
ceyokota <ceyokota@ucdavis.edu>
2026-03-17 21:10:38 -0700

    made volume a little lower on game on

--- game/v2-game-on-final/script.js
+++ game/v2-game-on-final/script.js
@@ -21,12 +21,13 @@
     for (let i = 0; i < buttons.length; i++) {
       buttons[i].addEventListener('click', function() {
         document.querySelector('#clickSound').currentTime = 0.2;
+        document.querySelector('#clickSound').volume = 0.8;
         document.querySelector('#clickSound').play();
       });
     }
 
     //making music a lil quieter
-    document.querySelector('#bgMusic').volume = 0.5; 
+    document.querySelector('#bgMusic').volume = 0.3; 
 
     //press button to start music
     document.querySelector('button').addEventListener('click',function(){
